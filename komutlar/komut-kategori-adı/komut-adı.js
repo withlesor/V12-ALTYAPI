accf8cc271952637620430eafedffaddd9a19f3c
BTHANYLMAZER <51192419+BTHANYLMAZER@users.noreply.github.com>
2020-05-29 15:28:08 +0300

    Create komut-adı.js

---
+++ komutlar/komut-kategori-adı/komut-adı.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'komut-adı',
+  aliases: ['komut-değişkenleri'],
+  category: 'komut-kategorisi',
+  
+  run: async (app, message, args) => {
+    // Komut ayarları.
+  }
+}
